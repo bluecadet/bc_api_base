ee11d0fb8f8d6b3a1b2b55d8a2d3f401744a37ff
Pete Inge <pete.inge@gmail.com>
2020-02-10 16:39:06 -0500

    Fix cinder text filtering so we don't have br and line breaks

--- src/Plugin/Platform/CinderPlatform.php
+++ src/Plugin/Platform/CinderPlatform.php
@@ -16,4 +16,18 @@ namespace Drupal\bc_api_base\Plugin\Platform;
  */
 class CinderPlatform extends PlatformBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function applyPlatformTransformations(string $text) {
+
+    $new_text = $text;
+    $new_text = strip_tags($text, CINDER_ALLOWED_TAGS);
+    $new_text = preg_replace("/\r?\n|\r/", "", $new_text);
+
+    $new_text = trim($new_text);
+
+    return $new_text;
+  }
+
 }
