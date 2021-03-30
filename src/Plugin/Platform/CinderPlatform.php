734427653601667387213197dea10aabc16833b5
Pete Inge <pete.inge@gmail.com>
2021-03-30 16:33:54 -0400

    cinder tweaks

--- src/Plugin/Platform/CinderPlatform.php
+++ src/Plugin/Platform/CinderPlatform.php
@@ -22,10 +22,12 @@ class CinderPlatform extends PlatformBase {
   public function applyPlatformTransformations(string $text) {
 
     $new_text = $text;
-    $new_text = strip_tags($text, CINDER_ALLOWED_TAGS);
+    $new_text = preg_replace('#<p(.*?)>(.*?)</p>#is', '$2<br/>', $new_text);
     $new_text = preg_replace("/\r?\n|\r/", "", $new_text);
+    $new_text = strip_tags($new_text, CINDER_ALLOWED_TAGS);
 
     $new_text = trim($new_text);
+    $new_text = rtrim($new_text, "<br/>");
 
     return $new_text;
   }
